05429a573bf2b207e2b3e60b8a748fba3c3d4258
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-12 22:29:09 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="header">
       <img src="../images/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Project</h4>
+      <h4 className="header--project">Fun React Project</h4>
     </header>
   );
 }
