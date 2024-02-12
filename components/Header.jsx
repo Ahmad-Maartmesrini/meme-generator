f468c45b9915b3ade1d5f429eb21e84c9a20be2d
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-12 22:27:08 +0300

    add Troll Face

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
+      <img src="../images/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course</h4>
+      <h4 className="header--project">React Project</h4>
     </header>
   );
 }
