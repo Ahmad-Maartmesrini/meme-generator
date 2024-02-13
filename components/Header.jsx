29d0b81a53dd16acc84556ed199d379aca610b5f
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-13 08:50:04 +0300

    fix the src of the fav icon and logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="../src/assets/troll-face.png" className="header--image" />
+      <img src="./public/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">Fun React Project</h4>
     </header>
